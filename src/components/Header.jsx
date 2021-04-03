d48337aadc77f9d182c20f95bf1b2ce92a1c6678
Haron Acosta <haron.acosta14@gmail.com>
2021-04-03 09:49:16 -0400

    Project bases

---
+++ src/components/Header.jsx
@@ -0,0 +1,18 @@
+import React from 'react';
+
+const Header = () => {
+  return (
+    <header className="InteractiveHeader">
+      <div className="InteractiveHeader__title">
+        Simple, traffic-based pricing
+      </div>
+
+      <div className="InteractiveHeader__subtitle">
+        <p>Sign-up for our 30-day trial.</p>
+        <p>Not credit card required.</p>
+      </div>
+    </header>
+  );
+};
+
+export default Header;
---
+++ src/components/Pricing.jsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const Pricing = (props) => {
+  return <div className="Pricing">Pricing</div>;
+};
+
+export default Pricing;
