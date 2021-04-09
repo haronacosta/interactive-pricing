ca416198083c6581cd79abb081f426da0ec62580
Haron Acosta <haron.acosta14@gmail.com>
2021-04-09 15:23:35 -0400

    Range logic

--- src/components/Pricing.jsx
+++ src/components/Pricing.jsx
@@ -1,7 +1,47 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import prices from '../data/prices';
 
-const Pricing = (props) => {
-  return <div className="Pricing">Pricing</div>;
+const Pricing = () => {
+  const [price, setPrice] = useState(1);
+
+  const [selectPrice, setSelectPrice] = useState({});
+
+  useEffect(() => {
+    const priceRecive = parseInt(price);
+
+    const newPrice = prices.filter((priceData) => priceData.id === priceRecive);
+
+    setSelectPrice(newPrice[0]);
+  }, [price]);
+
+  return (
+    <>
+      <div className="Pricing">
+        <input
+          type="range"
+          className="Pricing__range"
+          value={price}
+          list="pricingmarks"
+          min="1"
+          max="5"
+          step="1"
+          onChange={(e) => {
+            setPrice(e.target.value);
+          }}
+        />
+
+        <datalist id="pricingmarks">
+          <option value="1"></option>
+          <option value="2"></option>
+          <option value="3"></option>
+          <option value="4"></option>
+          <option value="5"></option>
+        </datalist>
+
+        <div className="Pricing__select-price">{selectPrice.perMouth}</div>
+      </div>
+    </>
+  );
 };
 
 export default Pricing;
---
+++ src/data/prices.js
@@ -0,0 +1,29 @@
+const prices = [
+  {
+    id: 1,
+    pageviews: '10k',
+    perMouth: '8$',
+  },
+  {
+    id: 2,
+    pageviews: '50k',
+    perMouth: '12$',
+  },
+  {
+    id: 3,
+    pageviews: '100k',
+    perMouth: '16$',
+  },
+  {
+    id: 4,
+    pageviews: '500k',
+    perMouth: '24$',
+  },
+  {
+    id: 5,
+    pageviews: '1M',
+    perMouth: '36$',
+  },
+];
+
+export default prices;
